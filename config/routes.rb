4a56049fa4f2dd473c2e95fe749fc0b2cdb3115d
newbalance330 <92163228+newbalance330@users.noreply.github.com>
2021-11-22 04:13:16 +0000

    [Add]about追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
  devise_for :users
  root to: 'homes#top'
+ get 'about' => 'homes#about'
  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
  resources :users, only: [:new, :create, :index, :show, :edit, :update]
 
